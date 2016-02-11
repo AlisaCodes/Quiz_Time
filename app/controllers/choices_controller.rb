08ed826a27bef719b95812a5aa6071298085eb5f
Ali Colo <alisaclone@gmail.com>
2016-02-11 14:00:47 -0800

    add the ability to add choices to questions

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -3,4 +3,19 @@ class ChoicesController < ApplicationController
     @question = Question.find(params[:question_id])
     @choice = @question.choices.new
   end
+
+  def create
+    @question = Question.find(params[:question_id])
+    @choice = @question.choices.new(choice_params)
+    if @choice.save
+      redirect_to survey_path(@question.survey)
+    else
+      render :new
+    end
+  end
+
+private
+  def choice_params
+    params.require(:choice).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   resources :surveys do
     resources :questions
   end
+  resources :questions do
+    resource :choices
+  end
 end
